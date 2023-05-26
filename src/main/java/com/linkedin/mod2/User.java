ea55c43ff2ac744f75ed538569e11d42898ffd84
BrightBoost <info@brightboost.nl>
2023-05-26 20:15:29 +0200

    Back to initial state User.java

--- src/main/java/com/linkedin/mod2/User.java
+++ src/main/java/com/linkedin/mod2/User.java
@@ -4,12 +4,7 @@ public class User {
     private int id;
     private String name;
 
-    /**
-     * Retrieves a user based on their unique ID.
-     *
-     * @param id the unique ID of the user
-     * @return the user with the specified ID, or null if not found
-     */
+    // This method gets the user by ID
     public User getUserById(int id) {
         // implementation omitted
         return null;
